9fdb4586c212e2b5a3312f044c564495a373f9dc
MalligaSaravanan93 <malligasaravanan93@gmail.com>
2022-03-04 09:24:06 +0000

    Interview task

--- app/src/main/java/com/malliga/interviewtask/utils/UIState.kt
+++ app/src/main/java/com/malliga/interviewtask/utils/UIState.kt
@@ -3,7 +3,7 @@ package com.malliga.interviewtask.utils
 import com.malliga.interviewtask.model.CardData
 
 sealed class UIState {
-    class LOADING(val isLoading: Boolean = true) : UIState()
     class SUCCESS(val success: CardData): UIState()
+    class LOADING(val isLoading: Boolean = true) : UIState()
     class ERROR(val error: Throwable): UIState()
 }
